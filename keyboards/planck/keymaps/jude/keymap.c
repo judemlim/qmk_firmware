e0f23c6b9b9a8257c1791bcebc4be520465c612a
judemlim <judemlim@gmail.com>
2023-06-06 22:33:34 +1000

    update cut copy and paste

--- keyboards/planck/keymaps/jude/keymap.c
+++ keyboards/planck/keymaps/jude/keymap.c
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_EXT] = LAYOUT_planck_grid(
     KC_ESC,  KC_F1, KC_F2,  KC_F3, KC_F4,  KC_F5, KC_F6,  KC_F7, KC_F8,  KC_F9, KC_F10,  KC_F11,
-    KC_GRV, KC_1,  KC_CUT,  KC_COPY,  KC_PASTE,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_F12,
+    KC_GRV, KC_1,  C(KC_X),  C(KC_C),  C(KC_V),  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_F12,
     _______, _______,   KC_HOME,   KC_END,   KC_DELETE,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
