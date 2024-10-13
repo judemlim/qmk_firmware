2c53b6f499e411f4685a2b1ae678cef935cd692f
judemlim <judemlim@gmail.com>
2024-10-13 13:46:35 +1100

    mac copy paste shortcut

--- keyboards/planck/keymaps/jude/keymap.c
+++ keyboards/planck/keymaps/jude/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
     CTL_T(KC_ESC),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINUS,
     KC_LSFT, LT(_EXT, KC_SCLN), KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-    KC_RIGHT_ALT, KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,  CTL_T(KC_ENT),  LT(_LOWER, KC_BSPC),  LT(_RAISE, KC_TAB),   ALT_T(KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT
+    KC_RIGHT_ALT, KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,  ALT_T(KC_ENT),  LT(_LOWER, KC_BSPC),  LT(_RAISE, KC_TAB),   CTL_T(KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_EXT] = LAYOUT_planck_grid(
     KC_ESC,  KC_F1, KC_F2,  KC_F3, KC_F4,  KC_F5, KC_F6,  KC_F7, KC_F8,  KC_F9, KC_F10,  KC_F11,
-    KC_GRV, KC_1,  C(KC_X),  C(KC_C),  C(KC_V),  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_F12,
-    _______, _______,   KC_HOME,   KC_END,   KC_DELETE,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    KC_GRV, KC_1,  G(KC_X),  G(KC_C),  G(KC_V),  G(KC_B),  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_F12,
+    _______, _______,   C(KC_X),   C(KC_C),   C(KC_V),  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
